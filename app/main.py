b510bd00ea00e849807008452f9b78e3bece969c
Alex <garbieto@fi.uba.ar>
2024-04-16 22:20:08 -0300

    predominant emotion detected

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from deepface import DeepFace
+from pydantic import BaseModel
+import base64
+
+
+class Image(BaseModel):
+    base64_str: str
 
 app = FastAPI()
 
@@ -11,3 +18,28 @@ def read_root():
 @app.get("/hello/{name}")
 def hello_name(name: str = "World"):
     return {"message": f"Hello, {name}!"}
+
+
+@app.get("/emotion")
+def test():
+    with open('./images/happy_face.jpg', 'rb') as file:
+        image_data = file.read()
+        base64_encoded_image = base64.b64encode(image_data)
+        base64_string = base64_encoded_image.decode('utf-8')
+
+        return {
+            "emotion": DeepFace.analyze(
+                img_path = f"data:image/jpeg;base64,{base64_string}", 
+                actions = ['emotion']
+            )[0]["dominant_emotion"]
+        }
+
+
+@app.post("/dominant_emotion", status_code=200)
+def get_dominant_emotion(image: Image):
+    dominant_emotion = DeepFace.analyze(
+        img_path = image.base64_str, 
+        actions = ['emotion']
+    )[0]["dominant_emotion"]
+
+    return { "dominant_emotion": dominant_emotion }
--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
 from .main import app
+import base64
+
 
 client = TestClient(app)
 
@@ -8,6 +10,7 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "It's alive!"}
+
 
 def test_hello_name_with_world():
     response = client.get("/hello/World")
@@ -19,3 +22,23 @@ def test_hello_name_with_alex():
     response = client.get("/hello/Alex")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, Alex!"}
+
+
+def test():
+    response = client.get("/emotion")
+    assert response.status_code == 200
+    assert response.json() == {"emotion": "happy"}
+
+
+def test_get_dominant_emotion():
+    with open('./images/happy_face.jpg', 'rb') as file:
+        image_data = file.read()
+        base64_encoded_image = base64.b64encode(image_data)
+        base64_string = base64_encoded_image.decode('utf-8')
+
+        response = client.post("/dominant_emotion", json={
+            "image": f"data:image/jpeg;base64,{base64_string}"
+        })
+
+        assert response.status_code == 200
+        assert response.json() == {"dominant_emotion": "happy"}
